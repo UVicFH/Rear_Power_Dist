056318f47c718c5384467603524297df41132a87
Marc Laventure <marclave@uvic.ca>
2017-04-19 17:19:25 -0700

    Added Header File

---
+++ rearPowerDistController.h
@@ -0,0 +1,20 @@
+#define SPI_CS_PIN 10
+#define CAN_REAR_POWER_DIST_MSG_ADDRESS 0x50
+
+#define CAN_0 0
+#define CAN_1 1
+#define CAN_2 2
+#define CAN_3 3
+#define CAN_4 4
+#define CAN_5 5
+#define CAN_6 6
+#define CAN_7 7
+
+#define BIT_1_MASK 0x1
+#define BIT_2_MASK 0x2
+#define BIT_3_MASK 0x4
+#define BIT_4_MASK 0x8
+#define BIT_5_MASK 0x10
+#define BIT_6_MASK 0x20
+#define BIT_7_MASK 0x40
+#define BIT_8_MASK 0x80
