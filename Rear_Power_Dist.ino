6d9c3403066e77774c93462fc62afe04c68723c0
Chad McColm <chadjmccolm@gmail.com>
2018-03-26 00:23:03 -0700

    Removed serial output
    Tested the board. Pins do what they're told when they're told to. Removed serial output because it dragged the TX pin high which kept downshift on. Now theoretically ready to run on the car.

--- Rear_Power_Dist.ino
+++ Rear_Power_Dist.ino
@@ -9,22 +9,39 @@ void set_outputs(byte len, byte* buf)
 {
   
   // Set each digital
+  if(buf[BRAKE_LIGHT_BYTE]>>BRAKE_LIGHT_BIT&MASK_1) Serial.println("Brake Activated!");
   digitalWrite(BRAKE_LIGHT_PIN, buf[BRAKE_LIGHT_BYTE]>>BRAKE_LIGHT_BIT&MASK_1);
+
+  if(buf[FUEL_PUMP_BYTE]>>FUEL_PUMP_BIT&MASK_1) Serial.println("Fuel Pump Activated!");
   digitalWrite(FUEL_PUMP_PIN, buf[FUEL_PUMP_BYTE]>>FUEL_PUMP_BIT&MASK_1);
+
+  if(buf[MS_BYTE]>>MS_BIT&MASK_1) Serial.println("Microsquirt Activated!");
   digitalWrite(MS_PIN, buf[MS_BYTE]>>MS_BIT&MASK_1);
+  
+  if(buf[BRAKE_SOL_BYTE]>>BRAKE_SOL_BIT&MASK_1) Serial.println("Brake Solenoid Activated!");
   digitalWrite(BRAKE_SOL_PIN, buf[BRAKE_SOL_BYTE]>>BRAKE_SOL_BIT&MASK_1);
+
+  if(buf[STARTER_BYTE]>>STARTER_BIT&MASK_1) Serial.println("Starter Activated!");
   digitalWrite(STARTER_PIN, buf[STARTER_BYTE]>>STARTER_BIT&MASK_1);
+
+  if(buf[SHIFT_UP_BYTE]>>SHIFT_UP_BIT&MASK_1) Serial.println("Upshift Activated!");
   digitalWrite(SHIFT_UP_PIN, buf[SHIFT_UP_BYTE]>>SHIFT_UP_BIT&MASK_1);
+
+  if(buf[SHIFT_DN_BYTE]>>SHIFT_DN_BIT&MASK_1) Serial.println("Downshift Activated!");
   digitalWrite(SHIFT_DN_PIN, buf[SHIFT_DN_BYTE]>>SHIFT_DN_BIT&MASK_1);
+
+  if(buf[SPARE_CPBRB_BYTE]>>SPARE_CPBRB_BIT&MASK_1) Serial.println("Spare CPBRB Activated!");
   digitalWrite(SPARE_CPBRB_PIN, buf[SPARE_CPBRB_BYTE]>>SPARE_CPBRB_BIT&MASK_1);
 
   // Set the engine fan
+  if(buf[2] > 0) Serial.println("Fan Activated");
   analogWrite(ENGINE_FAN_PIN, buf[2]);
 }
 
 void setup(){
 
-  Serial.begin(9600);
+  // Uncomment below line if you are debugging and need to see what is being received. Note this may keep the downshift and brake light on beacuse they're on the TX/RX pins respectively
+  // Serial.begin(9600);
   SPI.begin();
   
   // Create an infinite loop to prevent the program from starting before CAN is established
@@ -60,13 +77,11 @@ void loop(){
   // If there is a message available in the buffer
   while (CAN_MSGAVAIL == CAN.checkReceive())
   {
-    Serial.println("CAN Recieved");
+    
     // Create a buffer and insert the three byte message into it
     byte buf[8];
     byte len = 0;
     CAN.readMsgBuf(&len, buf);
-
-    Serial.println("Message Recieved: " + buf[0]);
 
     // Set the outputs of the board given the CAN message
     set_outputs(len, buf);
