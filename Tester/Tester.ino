1c2b421811ae71e6f76a06e073b30a093c5ae718
Chad McColm <chadjmccolm@gmail.com>
2018-03-27 14:33:20 -0700

    removed RTD noise

--- Tester/Tester.ino
+++ Tester/Tester.ino
@@ -24,7 +24,7 @@ void two_second_send(byte * data){
   
   for(int i = 0; i < 20; i++){
     CAN.sendMsgBuf(0x50, 0, 3, data);
-    Serial.println("Message Sent");
+    // Serial.println("Message Sent");
     delay(100);
   }
   
@@ -34,9 +34,9 @@ void loop()
 {
   // Create a placeholder for the message to be sent
   byte message[3] = {0,0,0};
-
-  Serial.println("Blank Message Initialized!");
-  
+//
+//  Serial.println("Blank Message Initialized!");
+//  
   // Cycle power to each shifter, the starter, the spare, the ready to go, the microsquirt, the fuel pump, and brake light for 2 seconds
   message[0] = 0b10000000;
   Serial.println("Shift Down!");
@@ -54,12 +54,8 @@ void loop()
   Serial.println("Spare1!");
   two_second_send(message);
 
-  message[0] >>= 1;
-  Serial.println("RTD!");
-  two_second_send(message);
-
-  message[0] >>= 1;
-  Serial.println("MS!");
+  message[0] >>= 2;
+  Serial.println("MicroSquirt!");
   two_second_send(message);
 
   message[0] >>= 1;
@@ -69,23 +65,36 @@ void loop()
   message[0] >>= 1;
   Serial.println("Brake Light!");
   two_second_send(message);
-  
-  // Turn everything off
-  message[0] = 0b0;
-  two_second_send(message);
-
+//  
+//  // Turn everything off
+//  message[0] = 0b0;
+//  two_second_send(message);
+//
   // Ramp up the engine fan and bring it back down again in two seconds
-  Serial.println("Fan Up!");
-  for(int i = 0; i < 255; i++){
-    message[2] = i;
-    CAN.sendMsgBuf(ID, 0, 3, message);
-    delay(8);
-  }
-  Serial.println("Fan Down!");
-  for(int i = 254; i >=0; i--){
-    message[2] = i;
-    CAN.sendMsgBuf(ID, 0, 3, message);
-    delay(8);
-  }
+//  Serial.println("Fan Up!");
+//  for(int i = 0; i < 160; i++){
+//    message[2] = i;
+//    CAN.sendMsgBuf(ID, 0, 3, message);
+//    delay(16);
+//  }
+//  Serial.println("Fan Down!");
+//  for(int i = 160; i >=0; i--){
+//    message[2] = i;
+//    CAN.sendMsgBuf(ID, 0, 3, message);
+//    delay(16);
+//  }
+//
+//  message[2]=160;
+//  Serial.println("Fan on");
+//  two_second_send(message);
+//
+//  delay(10000);
+//
+//  message[2] = 0;
+//  Serial.println("Fan off");
+//  two_second_send(message);
+//
+//  delay(6000);
+  
 
 }
